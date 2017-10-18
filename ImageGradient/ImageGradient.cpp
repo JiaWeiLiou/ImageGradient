610fca9c9ecc4e95b040022c2577f32dddb0b475
JiaWeiLiou <j0939115399@gmail.com>
2017-10-18 11:16:19 +0800

    ImageGradient

--- ImageGradient/ImageGradient.cpp
+++ ImageGradient/ImageGradient.cpp
@@ -28,16 +28,47 @@ int main()
 
 	/*載入原圖*/
 
-	Mat srcImage = imread(infile, 0);
+	Mat srcImage = imread(infile);	//原始圖
 	if (!srcImage.data) { printf("Oh，no，讀取srcImage錯誤~！ \n"); return false; }
+
+	/*將原圖像轉換為灰度圖像*/
+
+	Mat grayImage;
+	if (srcImage.type() != CV_8UC1) 
+	{ 
+		cvtColor(srcImage, grayImage, CV_BGR2GRAY);
+
+		string grayoutfile = filepath + "\\" + infilename + "_GRAY.png";		//灰階影像
+		imwrite(grayoutfile, grayImage);
+	}
+	else
+		grayImage = srcImage;
+
+
+
+	/*將灰度圖像中值模糊*/
+
+	Mat blurImage;
+	medianBlur(grayImage, blurImage, 9);
+
+	string bluroutfile = filepath + "\\" + infilename + "_BLUR.png";		//模糊影像
+	imwrite(bluroutfile, blurImage);
+
+	/*直方圖等化*/
+
+	Mat equalizeImage;
+	equalizeHist(blurImage, equalizeImage);
+
+	string equaloutfile = filepath + "\\" + infilename + "_EQUAL.png";		//模糊影像
+	imwrite(equaloutfile, equalizeImage);
 
 	/*計算梯度場*/
 
 	Mat grad_x;		//水平方向梯度
 	Mat grad_y;		//垂直方向梯度
-	//Differential(srcImage, grad_x, grad_y);	//計算梯度場
-	Sobel(srcImage, grad_x, CV_16S, 1, 0, 3, 1, 0, BORDER_DEFAULT);
-	Sobel(srcImage, grad_y, CV_16S, 0, 1, 3, 1, 0, BORDER_DEFAULT);
+	//Differential(equalizeImage, grad_x, grad_y);	//計算梯度場
+	Sobel(equalizeImage, grad_x, CV_16S, 1, 0, 3, 1, 0, BORDER_DEFAULT);
+	Sobel(equalizeImage, grad_y, CV_16S, 0, 1, 3, 1, 0, BORDER_DEFAULT);
 
 	Mat gradientField;	//結合水平及垂直方向梯度為梯度場
 	GradientField(grad_x, grad_y, gradientField);
@@ -46,17 +77,17 @@ int main()
 	Mat gradient_dir;		//計算梯度方向(0-360)
 	CalculateGradient(gradientField, gradient_mag, gradient_dir);
 
-	Mat gradientImage_col;		//輸出用色環梯度場
+	Mat gradientImage_col;			//輸出用色環梯度場
 	DrawMunsellColorSystem(gradientField, gradientImage_col);
 	string gradOutfile_col = filepath + "\\" + infilename + "_GRAD(color).png";		//梯度場(色環)
 	imwrite(gradOutfile_col, gradientImage_col);
 
-	Mat gradientImage_abs;		//輸出用絕對值梯度場
+	Mat gradientImage_abs;			//輸出用絕對值梯度場
 	DrawAbsGraySystem(gradientField, gradientImage_abs);
 	string gradOutfile_abs = filepath + "\\" + infilename + "_GRAD(abs).png";		//梯度場(絕對值)
 	imwrite(gradOutfile_abs, gradientImage_abs);
 
-	Mat gradientImage_com;		//輸出用疊合梯度場
+	Mat gradientImage_com;			//輸出用疊合梯度場
 	DrawAbsGraySystemAtImage(gradientImage_abs, srcImage, gradientImage_com, 2);
 	string gradOutfile_com = filepath + "\\" + infilename + "_GRAD(com).png";		//梯度場(疊合)
 	imwrite(gradOutfile_com, gradientImage_com);
@@ -66,12 +97,12 @@ int main()
 	Mat NMSgradientField;
 	NonMaximumSuppression(gradientField, NMSgradientField);
 
-	Mat NMSgradientField_col;	//輸出用非最大值抑制色環梯度場
+	Mat NMSgradientField_col;		//輸出用非最大值抑制色環梯度場
 	DrawMunsellColorSystem(NMSgradientField, NMSgradientField_col);
 	string nmsOutfile_col = filepath + "\\" + infilename + "_NMS(color).png";		//非極大值抑制(色環)
 	imwrite(nmsOutfile_col, NMSgradientField_col);
 
-	Mat NMSgradientField_abs;	//輸出用非最大值抑制絕對值梯度場
+	Mat NMSgradientField_abs;		//輸出用非最大值抑制絕對值梯度場
 	DrawAbsGraySystem(NMSgradientField, NMSgradientField_abs);
 	string nmsOutfile_abs = filepath + "\\" + infilename + "_NMS(abs).png";			//非極大值抑制(絕對值)
 	imwrite(nmsOutfile_abs, NMSgradientField_abs);
@@ -98,7 +129,7 @@ int main()
 	string atOutfile_abs = filepath + "\\" + infilename + "_HT(abs).png";		//滯後閥值(絕對值)
 	imwrite(atOutfile_abs, HTedge_abs);
 
-	Mat HTedge_com;		//輸出用滯後閥值疊合梯度場
+	Mat HTedge_com;					//輸出用滯後閥值疊合梯度場
 	DrawEdgeSystemAtImage(HTedge, srcImage, HTedge_com);
 	string atOutfile_com = filepath + "\\" + infilename + "_HT(com).png";		//滯後閥值(疊合)
 	imwrite(atOutfile_com, HTedge_com);
